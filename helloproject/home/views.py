94d4fe5ea69b8c79a684551f12162134acadc2c8
ZenTeknik <arainharis151@gmail.com>
2024-11-09 18:57:24 +0500

    NEW

--- helloproject/home/views.py
+++ helloproject/home/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("This is Homepage ")
+
+
+def about(request):
+    return HttpResponse("hello  ! About ")
+
+def services(request):
+    return HttpResponse( "Services Page  ")
+
+
+def contact(request):
+    return HttpResponse( "Contact Page  ")
