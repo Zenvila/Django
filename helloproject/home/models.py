1fd484044d1767552aebfa1a9f7818db252154bf
ZenTeknik <arainharis151@gmail.com>
2024-11-12 22:35:35 +0500

    ERROR

--- helloproject/home/models.py
+++ helloproject/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = model.models.CharField(max_length=50)
+    pub_date = models.DataTimeField(' date published')
+    
+    def _str_(self):
+        return self.question_text
+    class Choice(models.Models ):
+        questions = models.ForeignKey(Question,on_delete=models.CASCADE)
+        choice_text =models.IntegerField(default=0)
